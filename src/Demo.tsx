df04b9be2e2a9d0e74f39201e145e93ca82214aa
lopo <lopo@zju.edu.cn>
2022-09-28 13:16:27 +0800

    test: demo page

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -3,48 +3,20 @@ import { RafController } from "./RafController";
 
 export default () => {
     const [ ctx, setCtx ] = useState<CanvasRenderingContext2D | null>(null)
+    const [ ctx2, setCtx2 ] = useState<CanvasRenderingContext2D | null>(null)
 
     useEffect(() => {
         const canvas = (document.getElementById('demoCanvas') as HTMLCanvasElement)!
-        canvas.width = canvas.clientWidth
-        canvas.height = canvas.clientHeight
+        const canvas2 = (document.getElementById('demoCanvas2') as HTMLCanvasElement)!
+
         setCtx(canvas.getContext('2d'))
+        setCtx2(canvas2.getContext('2d'))
         console.log('loaded')
     }, [])
 
-    const videoEl = document.createElement('video')
-    videoEl.autoplay = true
-    videoEl.playsInline = true
-    videoEl.width = 1
-    videoEl.height = 1
-    let rafId = -1
-    const rafLoop = (curr_t: DOMHighResTimeStamp) => {
-        ctx?.drawImage(videoEl, 0, 0)
-        rafId = requestAnimationFrame(rafLoop)
-    }
-
-    const doRaf = () => {
-        navigator
-            .mediaDevices
-            .getUserMedia({ video: true, audio: false })
-            .then(stream => {
-                videoEl.srcObject = stream
-                document.body.appendChild(videoEl)
-                rafLoop(performance.now())
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
-    const doStop = () => {
-        videoEl.srcObject = null
-        cancelAnimationFrame(rafId)
-    }
-
     const controller = new RafController()
     const controllerStart = () => {
-        controller.start(ctx!)
+        controller.start_detect(ctx!, ctx2!)
     }
     const controllerStop = () => {
         controller.stop()
@@ -52,12 +24,20 @@ export default () => {
 
     return (
         <div className="demo">
-            <button onClick={ doRaf }>do raf</button>
-            <button onClick={ doStop }>do stop</button>
-            <br/>
             <button onClick={ controllerStart }>controllerStart</button>
             <button onClick={ controllerStop }>controllerStop</button>
-            <canvas id="demoCanvas" width={ 600 } height={ 400 }/>
+            <canvas id="demoCanvas"
+                    style={ {
+                        position: 'absolute',
+                        zIndex: 1
+                    } }
+                    width={ 400 } height={ 300 }/>
+            <canvas id="demoCanvas2"
+                    style={ {
+                        position: 'absolute',
+                        zIndex: 10
+                    } }
+                    width={ 400 } height={ 300 }/>
         </div>
     )
 }
